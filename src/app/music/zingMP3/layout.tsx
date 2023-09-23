43dac52998f6a5ee3731506d8138f8288b7f89a4
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2023-09-23 10:11:31 +0700

    deopted

--- src/app/music/zingMP3/layout.tsx
+++ src/app/music/zingMP3/layout.tsx
@@ -2,14 +2,17 @@ import PageTransition from "~/components/page-transition";
 import { ZingMP3ContextProvider } from "~/contexts/zing-mp3-context";
 import { ChildReact } from "~/types/type";
 import ZingMP3Empty from "./components/zing-mp3-empty";
+import { Suspense } from "react";
 
 export default function Layout({ children }: ChildReact) {
   return (
     <PageTransition>
-      <ZingMP3ContextProvider>
-        {children}
-        <ZingMP3Empty />
-      </ZingMP3ContextProvider>
+      <Suspense>
+        <ZingMP3ContextProvider>
+          {children}
+          <ZingMP3Empty />
+        </ZingMP3ContextProvider>
+      </Suspense>
     </PageTransition>
   );
 }

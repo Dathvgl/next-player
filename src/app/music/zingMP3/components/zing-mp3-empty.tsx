2b3da5bf18c8d3050c2376ed000ff4b5f0ef6c57
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2023-10-14 12:47:47 +0700

    music responsive

--- src/app/music/zingMP3/components/zing-mp3-empty.tsx
+++ src/app/music/zingMP3/components/zing-mp3-empty.tsx
@@ -5,5 +5,5 @@ import { useAppSelector } from "~/redux/hook";
 export default function ZingMP3Empty() {
   const id = useAppSelector((state) => state.music.zingMP3.current.id);
   if (!id) return <></>;
-  else return <div className="h-24" />;
+  else return <div className="md:h-24 max-md:h-40" />;
 }

40ce9605044199e302d91e744bb06e69800d4e9c
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-02-04 14:33:33 +0700

    remove

--- src/app/story/[type]/chapter/[mangaId]/[chapterId]/layout.tsx
+++
@@ -1,6 +0,0 @@
-import PageTransition from "~/components/page-transition";
-import { ChildReact } from "~/types/type";
-
-export default function Layout({ children }: ChildReact) {
-  return <PageTransition>{children}</PageTransition>;
-}

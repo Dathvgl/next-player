51f64e98e7c4a257fcafd137b52c3fc0632d0bc4
Dathvgl <99634251+Dathvgl@users.noreply.github.com>
2024-02-05 23:50:05 +0700

    api music

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,9 @@
 const linkApiCrawl = process.env.NEXT_PUBLIC_EXTERNAL_API_CRAWL;
-const linkApiFsfssfssfsfsfs =
-  process.env.NEXT_PUBLIC_EXTERNAL_API_FSFSSFSSFSFSFS;
 
 export const linkApi = {
   base: `${linkApiCrawl}`,
   role: `${linkApiCrawl}/api/role`,
   user: `${linkApiCrawl}/api/user`,
   manga: `${linkApiCrawl}/api/manga`,
-  musicZingMP3: `${linkApiFsfssfssfsfsfs}/api/zingMP3`,
+  music: `${linkApiCrawl}/api/music`,
 };
--- src/services/music-service.ts
+++ src/services/music-service.ts
@@ -10,24 +10,24 @@ import { ZingMP3Home, ZingMP3Search } from "~/types/music/zingMP3/zingMP3";
 
 export async function getZingMP3Home() {
   return await handleFetch<ZingMP3Home>({
-    url: `${linkApi.musicZingMP3}/home`,
+    url: `${linkApi.music}/zingMP3/home`,
   });
 }
 
 export async function getZingMP3Search(name: string) {
   return await handleFetch<ZingMP3Search>({
-    url: `${linkApi.musicZingMP3}/search/${name}`,
+    url: `${linkApi.music}/zingMP3/search/${name}`,
   });
 }
 
 export async function getZingMP3Detail(id: string) {
   return await handleFetch<ZingMP3SongResponse>({
-    url: `${linkApi.musicZingMP3}/infoSong/${id}`,
+    url: `${linkApi.music}/zingMP3/infoSong/${id}`,
   });
 }
 
 export async function getZingMP3Song(id: string) {
   return await handleFetch<ZingMP3SongDetailResponse>({
-    url: `${linkApi.musicZingMP3}/song/${id}`,
+    url: `${linkApi.music}/zingMP3/song/${id}`,
   });
 }
